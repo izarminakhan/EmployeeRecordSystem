f98230cd63b470c7d5dfe4baa2b9d99c6fee61e0
Ali Abdul Jabbar <aliabduljabbar@icloud.com>
2020-12-27 16:15:48 +0500

    Initial Commit

---
+++ EmployeeRecordSystem/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  EmployeeRecordSystem
+//
+//  Created by Ali Jabbar on 27/12/2020.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
